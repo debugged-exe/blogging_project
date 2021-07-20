56de2b696b74d2a0a1248207d2f6c82014217235
unknown <sumedhovhal765@gmail.com>
2021-07-20 10:54:16 +0530

    IconBg added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import IconBg from './components/IconBg/IconBg';
+import {FiFacebook} from "react-icons/fi";
+import {FaLinkedinIn} from "react-icons/fa";
+import {AiOutlineTwitter} from "react-icons/ai";
+import {AiFillInstagram} from "react-icons/ai";
+
+
+import "tachyons";
+
 
 function App() {
   return (
-    <div >
-      heyya
+    <div className="flex flex-wrap">
+      <IconBg icon={<FiFacebook size="4rem" className="bg-blue black shapeRoundedSquare iconStyling" />}  />
+      <IconBg icon={<AiOutlineTwitter size="4rem" className="bg-green black shapeCircle iconStyling " />}  />
+      <IconBg icon={<FaLinkedinIn size="4rem" className="bg-purple black shapeSquare iconStyling" />}  />
+      <IconBg icon={<AiFillInstagram size="4rem" className="bg-yellow black shapeRoundedSquare iconStyling" />}  />
     </div>
   );
 }
---
+++ src/components/IconBg/IconBg.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function IconBg({icon}){
+
+  return(
+      <div className="ma3">
+        <p>{icon}</p>
+      </div>
+
+
+  )
+}
+export default IconBg;
