97e7f1986da2abadf430cc7d2e845c3333c7524f
Sarah-2000 <sjdkazi@gmail.com>
2021-07-27 19:28:51 +0530

    updated RightDrawer,added TAGS and RECENT POSTS

--- src/components/RightDrawer/RightDrawer.jsx
+++ src/components/RightDrawer/RightDrawer.jsx
@@ -15,12 +15,45 @@ const RightDrawer = () => {
 
             <Newsletter />
 
+
             <h5>RECENT POSTS</h5>
             <div className="line" ></div>
 
             <div>
-                <h2>Why is it essential to have a resolve a issue that is important or maybe not i have no idea.</h2>
+                <h2><b>Why is it essential to have a resolve a issue that is important or maybe not i have no idea.</b></h2>
+                <div className="f6 light-silver" style={{ textAlign: "left", fontWeight: "bold" }}> 2 DAYS AGO  •  5 MIN READ </div>
+                <h2><b>Why is it essential to have a resolve a issue that is important or maybe not i have no idea.</b></h2>
+                <div className="f6 light-silver" style={{ textAlign: "left", fontWeight: "bold" }}> 19 DAYS AGO  •  4 MIN READ </div>
+                <h2><b>Why is it essential to have a resolve a issue that is important or maybe not i have no idea.</b></h2>
+                <div className="f6 light-silver" style={{ textAlign: "left", fontWeight: "bold" }}> 19 DAYS AGO  •  7 MIN READ </div>
             </div>
+
+            <br/>
+
+             <h5>TAGS</h5>
+             <div className="line" ></div>
+                 
+             <div style={{display:'flex',flexWrap:'wrap',flexDirection:'column',justifyContent:'space-between',textAlign:'left',height:'250px'}}>
+             <div>
+               <b>Web Design <br/><br/>
+                  Web Development <br/><br/>
+                  Entrepreneurship <br/><br/>
+                  Education <br/><br/>
+                  Startups
+                 </b>
+             </div>
+
+             <div>
+                <div className='circle'>365</div><br/>
+                <div className='circle'>102</div><br/>
+                <div className='circle'>41</div><br/>
+                <div className='circle'>33</div><br/>
+                <div className='circle'>29</div>
+             </div>
+
+             </div>
+             
+
         </div>
     );
 }
